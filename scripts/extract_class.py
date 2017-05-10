39a591b384191418672e557c3002245cfacb5c52
Joris van Vugt <jorisvan.vugt@student.ru.nl>
2017-05-10 12:42:22 +0200

    Add extension parameter

--- scripts/extract_class.py
+++ scripts/extract_class.py
@@ -7,7 +7,7 @@ import os
 import shutil
 from skimage import io
 
-def extract_class(class_name):
+def extract_class(class_name, extension='jpg'):
     src_dir = os.path.join(paths.DATA_FOLDER, 'train-jpg')
     dest_dir = os.path.join(paths.DATA_FOLDER, class_name)
     os.mkdir(dest_dir)
@@ -16,11 +16,11 @@ def extract_class(class_name):
     for image_name, image_labels in zip(label_csv.image_name.values, labels):
         if class_name in image_labels:
             img = io.imread(os.path.join(src_dir, image_name) + '.jpg')
-            io.imsave(os.path.join(dest_dir, image_name) + '.bmp', img)
+            io.imsave(os.path.join(dest_dir, image_name) + '.' + extension, img)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage:\npython %s class_name')
+    if len(sys.argv) != 3:
+        print('Usage:\npython %s class_name extension')
     else:
-        extract_class(sys.argv[1])
+        extract_class(sys.argv[1], sys.argv[2])
