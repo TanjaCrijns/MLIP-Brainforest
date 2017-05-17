fb4f67fd81f00d8e2b7f3b9aa274ddee82a6a069
Chris Kamphuis <mail@chriskamphuis.com>
2017-05-17 13:17:54 +0200

    extract mines functionality

--- scripts/data.py
+++ scripts/data.py
@@ -10,26 +10,29 @@ labels = ['agriculture', 'artisinal_mine', 'bare_ground', 'blooming',
           'habitation', 'haze', 'partly_cloudy', 'primary', 'road',
           'selective_logging', 'slash_burn', 'water']
 
+
 def get_class_data(train=True, label='weather'):
     '''
-    Returns either the train or validation data with the class you want to 
+    Returns either the train or validation data with the class you want to
     train. If the weather class all weather classes will be returned.
     The data is returned as a pandas dataframe.
     '''
     df = None
     if train:
         df = pd.read_csv(data_path + '/train.csv')
-    else: # validation if not train
+    else:  # validation if not train
         df = pd.read_csv(data_path + '/validation.csv')
 
     if label == 'weather':
         df = df[['image_name', 'clear', 'cloudy', 'haze', 'partly_cloudy']]
+    elif label == 'mines':
+        df = df[['image_name', 'artisinal_mine', 'conventional_mine']]
     elif label in labels:
         df = df[['image_name', label]]
     else:
-        raise ValueError('label should be a class label or weather') 
-    
+        raise ValueError('label should be a class label or weather')
     return df
+
 
 def get_data(train=True):
     '''
@@ -39,6 +42,6 @@ def get_data(train=True):
     df = None
     if train:
         df = pd.read_csv(data_path + '/train.csv')
-    else: # validation if not train
+    else:  # validation if not train
         df = pd.read_csv(data_path + '/validation.csv')
     return df
