94db67e6e490dfe709179a32fd0c20e725385a5a
Joris van Vugt <jorisvan.vugt@student.ru.nl>
2017-05-24 01:54:32 +0200

    Remove redundant file

--- rainforest/dataset.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-labels = ['agriculture', 'artisinal_mine', 'bare_ground', 'blooming', 'blow_down', 'clear', 'cloudy', 'conventional_mine', 'cultivation', 'habitation', 'haze', 'partly_cloudy', 'primary', 'road', 'selective_logging', 'slash_burn', 'water']
-train = pd.read_csv('../data/train.csv')
-validation = pd.read_csv('../data/validation.csv')
